4d15fcfc92208b96dc7741f96e0e082dca5e4888
Punit <jpunit2005@gmail.com>
2024-09-29 23:28:52 +0530

    Another commit

--- src/Components/Announcements.jsx
+++ src/Components/Announcements.jsx
@@ -26,15 +26,16 @@ const Announcements = () => {
     return (
         <div className="max-w-7xl mx-auto px-4 py-16" id="announcements">
             <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">Announcements</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {announcements.map((announcement) => (
-                    <div 
-                        key={announcement.id} 
-                        className="bg-[#e8e5ce] shadow-lg rounded-lg p-6 border  border-gray-300"
-                    >
-                        <h3 className="text-xl font-semibold mb-2 text-e04d46">{announcement.title}</h3>
-                        <p className="text-gray-500 mb-4">{announcement.date}</p>
-                        <p className="text-gray-700">{announcement.summary}</p>
+            <div className="relative">
+                <div className="absolute left-1/2 border-l border-gray-300 h-full"></div>
+                {announcements.map((announcement, index) => (
+                    <div key={announcement.id} className={`mb-8 flex items-start ${index % 2 === 0 ? 'flex-row-reverse' : ''}`}>
+                        <div className="bg-[#e8e5ce] p-4 shadow-lg rounded-lg border border-gray-300 w-1/2">
+                            <h3 className="text-xl font-semibold mb-2 text-[#e04d46]">{announcement.title}</h3>
+                            <p className="text-gray-500 mb-1">{announcement.date}</p>
+                            <p className="text-gray-700">{announcement.summary}</p>
+                        </div>
+                        <div className="w-4 h-4 rounded-full bg-[#e04d46] absolute left-1/2 transform -translate-x-1/2"></div>
                     </div>
                 ))}
             </div>
